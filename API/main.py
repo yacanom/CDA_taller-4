1c13c48c29c7713ecd6836cbcab117391302eaa4
David Polanía <david.polania3@gmail.com>
2022-12-06 23:27:30 -0500

    update

--- API/main.py
+++ API/main.py
@@ -2,7 +2,10 @@ from typing import List
 
 import pandas as pd
 
-from fastapi import FastAPI
+from sklearn.metrics import recall_score
+
+from fastapi import FastAPI, File, UploadFile
+
 
 from model import DataModel
 from prediction_model import PredictionModel
@@ -26,9 +29,53 @@ app.add_middleware(
 def read_root():
    return { "message": "Hello world" }
 
-@app.post("/retraining")
-def test():
-    return { "message": "Re entreno"}
+
+@app.post("/retrain")
+def retrain(file: UploadFile = File(...)):
+    df_original = pd.read_json('https://raw.githubusercontent.com/yacanom/CDA_taller-4/main/DataSet_Entrenamiento_v1.json')
+    
+    df_original['TotalCharges'] =  pd.to_numeric(df_original['TotalCharges'])
+    df_original['TotalCharges'] = df_original['TotalCharges'].fillna(df_original['TotalCharges'].median())
+    x_trainNew = df_original.drop(['customerID', 'Churn'],axis=1)
+    predicion_model = PredictionModel()
+    results = predicion_model.make_predictions(x_trainNew)
+    report_train = recall_score(df_original['Churn'], results, average='macro')
+    #---------------------------------------------------------------
+    df = pd.read_json(file.file)
+    df['TotalCharges'] =  pd.to_numeric(df['TotalCharges'])
+    df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+    x_trainNew = df.drop(['customerID', 'Churn'],axis=1)
+    y_trainNew = df['Churn']
+    fit_model = PredictionModel()
+    results = fit_model.make_fit(x_trainNew, y_trainNew)
+    
+    predicion_model2 = PredictionModel()
+    results = predicion_model2.make_predictions2(x_trainNew)
+    report_train2 = recall_score(df['Churn'], results, average='macro')
+    
+    return {"Recall: original":report_train, "Recall: nuevo":report_train2}
+    
+
+    
+@app.post("/predict2")
+def make_massive_predictions(file: UploadFile = File(...)):
+    df = pd.read_json(file.file)
+    df['TotalCharges'] =  pd.to_numeric(df['TotalCharges'])
+    df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+    x_trainNew = df.drop(['customerID', 'Churn'],axis=1)
+    predicion_model = PredictionModel()
+    proba = predicion_model.make_proba_predictions(x_trainNew)
+    #test_preds_proba = best_model.predict_proba(x_test)[:, 1]
+    #test_preds_proba = predicion_model.predict_proba(x_test)
+    predictions = predicion_model.make_predictions(x_trainNew)
+    predictions = pd.DataFrame(predictions, columns=['predictions'])
+    proba = pd.DataFrame(proba, columns=['Yes', 'No'])
+    predictions = pd.concat([predictions, proba], axis=1)
+    
+    
+    
+    #report = recall_score(df['Churn'], results, average='macro')
+    return {"predictions":predictions.to_dict(orient='records')}
 
 @app.post("/predict")
 def make_predictions(X: List[DataModel]):
--- API/prediction_model.py
+++ API/prediction_model.py
@@ -9,3 +9,24 @@ class PredictionModel:
     def make_predictions(self, data):
         result = self.model.predict(data)
         return result
+         
+    def make_proba_predictions(self, data):
+        result = self.model.predict_proba(data)
+        return result
+
+    def make_predictions2(self, data):
+        self.model2 = load("../model/modelo_nuevo.joblib")
+        result = self.model2.predict(data)
+        return result
+    
+    def make_fit(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+        #save model
+       
+        from joblib import dump
+        dump(self.model, "../model/modelo_nuevo.joblib")
+        
+        
+        return {"message": "Modelo reentrenado"} 
+        
+        
